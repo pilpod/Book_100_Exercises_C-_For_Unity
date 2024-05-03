86fcb9696628977e9e12bae8d12a8e77b583de3d
Giacomo C. <giacomo.corso@gmail.com>
2024-05-03 18:51:32 +0200

    ex 02 done

--- Assets/Scripts/Ex_02.cs
+++ Assets/Scripts/Ex_02.cs
@@ -8,10 +8,17 @@ public class Ex_02 : MonoBehaviour
      * mostrar por consola sus valores 
      * sumados, restados, multiplicados y divididos.*/
 
+    public float x = 2.0f;
+    public float y = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Addition: " + Addition());
+        Debug.Log("Subtraction: " + Subtraction());
+        Debug.Log("Multiplication: " + Multiplication());
+
+        Debug.Log((y != 0) ? "Division: " + Division() : "Error: You cannot divide by zero");
     }
 
     // Update is called once per frame
@@ -19,4 +26,13 @@ public class Ex_02 : MonoBehaviour
     {
         
     }
+
+    private float Addition() =>  x + y;
+
+    private float Subtraction() => x - y;
+
+    private float Multiplication() => x * y;
+
+    private float Division() => x / y;
+    
 }
