3a7f4038d23b9e3eba680cce8cd482cfa8e29478
Giacomo C. <giacomo.corso@gmail.com>
2024-05-03 18:24:34 +0200

    init book exercises

---
+++ Assets/Scripts/Ex_01.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_01 : MonoBehaviour
+{
+    public float velocity = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("The velocity is: " + velocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
