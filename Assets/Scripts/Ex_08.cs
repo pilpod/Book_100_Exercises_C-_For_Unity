c4047caf32182beb527e65a1d73fc6941a358140
Giacomo C. <giacomo.corso@gmail.com>
2024-05-08 18:36:31 +0200

    ex 08 done Time.deltaTime

--- Assets/Scripts/Ex_08.cs
+++ Assets/Scripts/Ex_08.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Ex_08 : MonoBehaviour
 {
+    /*
+     * Crear un script que imprima por consola, en cada fotograma, el tiempo que ha transcurrido desde el fotograma anterior.
+     */
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,9 @@ public class Ex_08 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Time.deltaTime : El tiempo en segundos que tardó en completarse el último frame (Read Only).
+        // Update + Time.deltaTime fundamental para realizar calculo y movimientos suaves y consistentes
+        float deltaTime = Time.deltaTime;
+        Debug.Log($"Tiempo transcurrido desde el fotograma anterior: {deltaTime}");
     }
 }
