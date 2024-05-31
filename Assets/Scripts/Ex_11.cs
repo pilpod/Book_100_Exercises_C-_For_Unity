887359732cceaf01b0ca2108155f681dfc1b47ea
Giacomo C. <giacomo.corso@gmail.com>
2024-05-31 20:12:51 +0200

    ex 11 done

--- Assets/Scripts/Ex_11.cs
+++ Assets/Scripts/Ex_11.cs
@@ -14,12 +14,19 @@ public class Ex_11 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Moving object in the scene
+        transform.position = transform.position + new Vector3(horizontalInput + movementSpeed * Time.deltaTime, 0, 0);
+        //transform.Translate(new Vector3(horizontalInput + movementSpeed * Time.deltaTime, 0, 0));
+
+        if (transform.position.x >= 10) transform.position = new Vector3(0, transform.position.y, 0);
+
     }
 }
