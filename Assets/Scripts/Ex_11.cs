cefce98a00b9a56626d49e5e18deb39ff88993ef
Giacomo C. <giacomo.corso@gmail.com>
2024-05-31 19:42:19 +0200

    init ex 11

---
+++ Assets/Scripts/Ex_11.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_11 : MonoBehaviour
+{
+    /*
+     * Crea un cubo que se desplace en el eje X a una velocidad de 1 unidad por segundo. 
+     * Al llegar a la posición 10, debe recolocarse en la posición 0
+     */
+
+    private float movementSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
