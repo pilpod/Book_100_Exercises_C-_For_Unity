21810773f890cb06694d65f1ea286a5c59ecbca6
Giacomo C. <giacomo.corso@gmail.com>
2024-06-01 21:29:29 +0200

    ex 12 done

--- Assets/Scripts/Ex_12.cs
+++ Assets/Scripts/Ex_12.cs
@@ -10,15 +10,23 @@ public class Ex_12 : MonoBehaviour
      * o a las letras A y D
      */
 
+    float movementSpeed = 45.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            float rotation = inputHorizontal + movementSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up, rotation);
+        }
     }
 }
