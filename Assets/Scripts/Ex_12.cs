ff1b7ad7bd6950532d0571947af2aadf609dc577
Giacomo C. <giacomo.corso@gmail.com>
2024-06-01 19:58:41 +0200

    init ex 12

---
+++ Assets/Scripts/Ex_12.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_12 : MonoBehaviour
+{
+    /*
+     * Haz que un cubo rote a la izquierda o a la derecha respondiendo
+     * a las pulsaciones del teclado correspondientes a los cursores <- , ->
+     * o a las letras A y D
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
