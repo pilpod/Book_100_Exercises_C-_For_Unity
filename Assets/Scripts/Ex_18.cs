9a2978772b6b80af308f0995c174afe6224495ee
Giacomo C. <giacomo.corso@gmail.com>
2024-06-12 16:10:21 +0200

    init ex 18

--- Assets/Scripts/Ex_18.cs
+++ Assets/Scripts/Ex_18.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Ex_18 : MonoBehaviour
 {
+    /*
+     * Crea un cubo que se destruya a los tres segundos de clicar en él.
+     */
+
     // Start is called before the first frame update
     void Start()
     {
