3b0d37e0a39ebf5389f3ed1a0018bbdb7a3b7030
Giacomo C. <giacomo.corso@gmail.com>
2024-06-12 15:46:39 +0200

    init ex 17

---
+++ Assets/Scripts/Ex_17.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_17 : MonoBehaviour
+{
+    /*
+     * Crea un cubo que se destruye al clicar en él.
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
