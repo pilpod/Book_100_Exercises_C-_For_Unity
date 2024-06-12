dcf2d85e8751677c7a62b490f5ffa9562e7bc8a3
Giacomo C. <giacomo.corso@gmail.com>
2024-06-12 16:28:43 +0200

    init ex 19

---
+++ Assets/Scripts/Ex_19.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_19 : MonoBehaviour
+{
+    /*
+     * Crea un cubo que se desactive entre
+     * tres y cinco segundos después de clicar en él
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
