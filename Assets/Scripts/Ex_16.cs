cf92ef46f5e4f6c69ba4c0305874aed2a566a6f5
Giacomo C. <giacomo.corso@gmail.com>
2024-06-12 15:43:30 +0200

    ex 16 done

--- Assets/Scripts/Ex_16.cs
+++ Assets/Scripts/Ex_16.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Ex_16 : MonoBehaviour
 {
+    /*
+     * Crea un objeto que se desplace en el eje Z
+     * una unidad al soltar la tecla espaciadora.
+     */
+
+    private float speed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,10 @@ public class Ex_16 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            //transform.position += Vector3.forward * speed;
+            transform.Translate(Vector3.forward * speed);
+        }
     }
 }
