e90f70761a014f5b3cc4f38821881c80a604a089
Giacomo C. <giacomo.corso@gmail.com>
2024-08-17 19:25:38 +0200

    ex 21 done. Pending optimization

--- Assets/Scripts/Ex_21.cs
+++ Assets/Scripts/Ex_21.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Ex_21 : MonoBehaviour
 {
+    /*
+     * Crea un bucle que sume los primeros quinientos números pares.
+     */
+
+    private int count = 0;
+    private int sum = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 1; i <= 1000; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sum += i;
+                count++;
+            };
+        }
+
+        Debug.Log(sum);
+        Debug.Log(count);
     }
 
     // Update is called once per frame
