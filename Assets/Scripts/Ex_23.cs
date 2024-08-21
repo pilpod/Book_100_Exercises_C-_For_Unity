9a98b9efc24643b323cb545551fea6a32f865301
Giacomo C. <giacomo.corso@gmail.com>
2024-08-21 17:00:42 +0200

    make a wall first solution done

--- Assets/Scripts/Ex_23.cs
+++ Assets/Scripts/Ex_23.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Ex_23 : MonoBehaviour
 {
+    /*
+     * Instanciar una pared de cubos vertical usando un prefab.
+     */
+
+    private GameObject[] wall;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int column = 1; column <= 5; column++)
+        {
+            for (int row = 1; row <= 5; row++)
+            {
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                go.transform.position = new Vector3(column, 0.5f * row, 0);
+            }
+            
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
