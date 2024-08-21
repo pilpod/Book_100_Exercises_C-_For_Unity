c1fe4ca7b550610f946c3748ccc02cb0b1405ed8
Giacomo C. <giacomo.corso@gmail.com>
2024-08-21 16:11:02 +0200

    ex 22 done

--- Assets/Scripts/Ex_22.cs
+++ Assets/Scripts/Ex_22.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Ex_22 : MonoBehaviour
 {
+    /*
+     * Crea una línea de diez cubos en el eje X usando un bucle, sin usar prefabs.
+     */
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 1; i <= 10; i++)
+        {
+            GameObject object1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            object1.transform.position = new Vector3(i * 2, 0.5f, 2);
+        }
+
     }
 
     // Update is called once per frame
