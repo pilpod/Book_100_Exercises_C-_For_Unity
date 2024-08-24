4071a78d58ad1218de562dd2db0ca624106d4da2
Giacomo C. <giacomo.corso@gmail.com>
2024-08-24 15:46:14 +0200

    ex 24 done. cube in 3D rendered

--- Assets/Scripts/Ex_24.cs
+++ Assets/Scripts/Ex_24.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Ex_24 : MonoBehaviour
 {
+    /*
+     * Instanciar un cubo hecho de cubos usando prefab.
+     */
+
+    public GameObject prefab;
+    private int cubeSize = 10;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int y = 0; y < cubeSize; y++)
+        {
+            for (int z = 0; z < cubeSize; z++)
+            {
+                for (int x = 0; x < cubeSize; x++)
+                {
+                    Vector3 position = new Vector3(x, y, z);
+                    Instantiate(prefab, position, Quaternion.identity);
+                }
+            }
+        }
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
