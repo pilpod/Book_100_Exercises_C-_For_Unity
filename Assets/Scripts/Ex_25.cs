8e06bc70ff1cd8a79ec734cd0be54d15a47db7a4
Giacomo C. <giacomo.corso@gmail.com>
2024-08-28 12:29:45 +0200

    init ex 25

---
+++ Assets/Scripts/Ex_25.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_25 : MonoBehaviour
+{
+    /*
+     * Instanciar un bloque de cubos como en el ejercicio 24, 
+     * pero utilizar diferentes prefabs para generar capas de piedra, tierra y césped,
+     * como si fuese una sección de suelo del juego Minecraft.
+     */
+
+    public GameObject rockPrefab;
+    public GameObject groundPrefab;
+    public GameObject grassPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
