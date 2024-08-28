4e40a5804f47479b36efd9ab9d17c6ec83d9da22
Giacomo C. <giacomo.corso@gmail.com>
2024-08-28 13:28:08 +0200

    ex done. pending refactor

--- Assets/Scripts/Ex_25.cs
+++ Assets/Scripts/Ex_25.cs
@@ -14,10 +14,35 @@ public class Ex_25 : MonoBehaviour
     public GameObject groundPrefab;
     public GameObject grassPrefab;
 
+    private int cubeSize = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int y = 0; y < cubeSize; y++)
+        {
+            for (int z = 0; z < cubeSize; z++)
+            {
+                for (int x = 0; x < cubeSize; x++)
+                {
+                    Vector3 position = new Vector3(x,y,z);
+                    
+                    if (y <= 5)
+                    {
+                        Instantiate<GameObject>(rockPrefab, position, Quaternion.identity);
+                        continue;
+                    }
+
+                    if (y <= 8 )
+                    {
+                        Instantiate<GameObject>(groundPrefab, position, Quaternion.identity);
+                        continue;
+                    }
+
+                    Instantiate<GameObject>(grassPrefab, position, Quaternion.identity);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
