e9fb495a3c902a5b68aa5e3fe91c9242e3f7fb0b
Giacomo C. <giacomo.corso@gmail.com>
2024-09-09 17:52:52 +0200

    ex 27 done

--- Assets/Scripts/Ex_27.cs
+++ Assets/Scripts/Ex_27.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,15 +10,37 @@ public class Ex_27 : MonoBehaviour
      * los puntos de un array o lista pública de vectores 3.
      */
 
+    private GameObject prefab;
+
+    public Vector3[] vectors;
+    public float velocity = 2.0f;
+    private int currentVector = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        prefab = GameObject.CreatePrimitive(PrimitiveType.Cube);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (vectors.Length == 0) return;
+
+        Vector3 direction = (vectors[currentVector] - prefab.transform.position).normalized;
         
+        float distance = Vector3.Distance(prefab.transform.position, vectors[currentVector]);
+
+        MovePrefab(direction);
+        
+        if (distance < 0.1f)
+        {
+            currentVector = (currentVector + 1) % vectors.Length;
+        }
+    }
+
+    private void MovePrefab(Vector3 direction)
+    {
+        prefab.transform.Translate(direction * velocity * Time.deltaTime, Space.World);
     }
 }
