eb721f292e5fc5043497e710da1219c670481e34
Giacomo C. <giacomo.corso@gmail.com>
2024-09-09 16:06:56 +0200

    init ex 27

---
+++ Assets/Scripts/Ex_27.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_27 : MonoBehaviour
+{
+    /*
+     * Crea un cubo que se desplace uniformemente entre
+     * los puntos de un array o lista pública de vectores 3.
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
