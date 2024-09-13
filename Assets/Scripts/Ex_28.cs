ab0135b7ac1e5fd983eff7c09cc428a0165841f0
Giacomo C. <giacomo.corso@gmail.com>
2024-09-13 20:08:32 +0200

    init ex 28

---
+++ Assets/Scripts/Ex_28.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_28 : MonoBehaviour
+{
+    /*
+     * Crea un cubo que se desplace uniformemente entre los puntos
+     * de un array o lista pública de Vector3, pero que elija
+     * aleatoriamente el siguiente punto de la colección
+     * al que desplazarse.
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
