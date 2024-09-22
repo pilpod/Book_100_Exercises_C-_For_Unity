8430d531ca1bfd411ef532c7c38a94ca7240a99d
Giacomo C. <giacomo.corso@gmail.com>
2024-09-22 17:51:03 +0200

    init ex 29

---
+++ Assets/Scripts/Ex_29.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_29 : MonoBehaviour
+{
+    /*
+     * Crea unos cuantos objetos con el tag <<desaparecer>> en la escena,
+     * manualmente. Al pulsar la tecla espaciadora, todos los objetos deben desativarse.
+     * Los objetos no pueden tener ningún script añadido. El ejercicio ha
+     * de funcionar sin modificar el código aunque se añadan o quiten objetos de la escena.
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
