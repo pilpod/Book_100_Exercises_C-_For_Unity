7239f50c3062058c8cd18227fdd082a38206d117
Giacomo C. <giacomo.corso@gmail.com>
2024-09-22 18:48:29 +0200

    ex 29 done with extra

--- Assets/Scripts/Ex_29.cs
+++ Assets/Scripts/Ex_29.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Ex_29 : MonoBehaviour
@@ -9,7 +11,11 @@ public class Ex_29 : MonoBehaviour
      * manualmente. Al pulsar la tecla espaciadora, todos los objetos deben desativarse.
      * Los objetos no pueden tener ningún script añadido. El ejercicio ha
      * de funcionar sin modificar el código aunque se añadan o quiten objetos de la escena.
+     * 
+     * Extra: Si los objectos fueron desactivados volver a activarlos también con la barra espaciadora.
      */
+
+    public GameObject[] spheres;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +26,26 @@ public class Ex_29 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (spheres.Length == 0) spheres = GameObject.FindGameObjectsWithTag("desaparecer");
+
+            foreach (var sphere in spheres)
+            {
+                ChangeStateObject(sphere);
+            }
+        }
+    }
+
+    private void ChangeStateObject(GameObject sphere)
+    {
+        if (sphere.activeSelf)
+        {
+            sphere.SetActive(false);
+        }
+        else
+        {
+            sphere.SetActive(true);
+        }
     }
 }
