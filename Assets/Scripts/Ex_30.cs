79d49135cde03fecc7512d67d375172e13be4399
Giacomo C. <giacomo.corso@gmail.com>
2024-09-29 15:49:12 +0200

    Instatiate cubes when game start

--- Assets/Scripts/Ex_30.cs
+++ Assets/Scripts/Ex_30.cs
@@ -9,16 +9,29 @@ public class Ex_30 : MonoBehaviour
      * en cada eje. Al pulsar el espacio, deben destruirse los que tengan posiciones
      * positivas en el eje x.
      */
-    
+
+    private readonly int _cubeQuantity = 10;
+    private readonly int _minRange = -10;
+    private readonly int _maxRange = 10;
+    public List<GameObject> cubes;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < _cubeQuantity; i++)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(
+                                            Random.Range(_minRange, _maxRange),
+                                            Random.Range(_minRange, _maxRange),
+                                            Random.Range(_minRange, _maxRange));
+            cubes.Add(cube);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
