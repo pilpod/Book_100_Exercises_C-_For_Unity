2fb71f1839c48637228a542bc6915de7453bc3b4
Giacomo C. <giacomo.corso@gmail.com>
2024-09-29 16:13:35 +0200

    ex 30 done

--- Assets/Scripts/Ex_30.cs
+++ Assets/Scripts/Ex_30.cs
@@ -8,6 +8,10 @@ public class Ex_30 : MonoBehaviour
      * Instancia diez cubos en la escena en posiciones aleatorias entre -10 y 10
      * en cada eje. Al pulsar el espacio, deben destruirse los que tengan posiciones
      * positivas en el eje x.
+     * 
+     * Se puede hacer el ejercicio creando GameObject publico y asignadole un tag. Seguidamente
+     * solo tendremos que crear 10 instancies del cubo publico con diferente posiciones tal como lo indica el
+     * paragrafo anterior. Utilizaremos FindGameObjectsWithTag para recuperar los cubos y destruirlos si se cumple la condición.
      */
 
     private readonly int _cubeQuantity = 10;
@@ -32,6 +36,15 @@ public class Ex_30 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (var cube in cubes)
+            {
+                if (cube.transform.position.x > 0)
+                {
+                    GameObject.Destroy(cube);
+                }
+            }
+        }
     }
 }
