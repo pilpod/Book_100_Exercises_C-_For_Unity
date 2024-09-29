6896f0326585b67ecb7b3011327297effec8776b
Giacomo C. <giacomo.corso@gmail.com>
2024-09-29 15:21:48 +0200

    init ex 30

---
+++ Assets/Scripts/Ex_30.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_30 : MonoBehaviour
+{
+    /*
+     * Instancia diez cubos en la escena en posiciones aleatorias entre -10 y 10
+     * en cada eje. Al pulsar el espacio, deben destruirse los que tengan posiciones
+     * positivas en el eje x.
+     */
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
