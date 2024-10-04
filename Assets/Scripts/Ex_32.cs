82f5042b9bccdc6e168c57bbff81f5e20d43f505
Giacomo C. <giacomo.corso@gmail.com>
2024-10-04 17:59:38 +0200

    ex 32 done

--- Assets/Scripts/Ex_32.cs
+++ Assets/Scripts/Ex_32.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,15 +10,32 @@ public class Ex_32 : MonoBehaviour
      * Al clicar sobre una, la otra cambia de color.
      */
 
+    public GameObject sphere2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void OnMouseDown()
+    {
+        ChangeColorOtherSphere();
+    }
+
+    public void ChangeColorOtherSphere()
+    {
+        if (sphere2 != null)
+        {
+            sphere2.GetComponent<Renderer>().material.color = Color.black;
+        }
+    }
+
+
 }
