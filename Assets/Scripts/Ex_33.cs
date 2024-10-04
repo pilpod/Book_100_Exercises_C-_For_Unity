34f22c5195373a10304c2ee1a0b9ee3d551068a7
Giacomo C. <giacomo.corso@gmail.com>
2024-10-04 18:09:12 +0200

    init ex 33

---
+++ Assets/Scripts/Ex_33.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_33 : MonoBehaviour
+{
+    /*
+     * Genera un efecto de movimiento modificando el offset
+     * de la textura de un material de un plane o quad.
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
