62ac8fd3f540cce2ebcd0f8cdc83fdad98bcbc06
Giacomo C. <giacomo.corso@gmail.com>
2024-10-04 17:51:30 +0200

    init ex 32

---
+++ Assets/Scripts/Ex_32.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_32 : MonoBehaviour
+{
+    /*
+     * Crear dos esferas. 
+     * Al clicar sobre una, la otra cambia de color.
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
