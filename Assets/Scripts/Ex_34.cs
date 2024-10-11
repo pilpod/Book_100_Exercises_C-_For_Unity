2147df7c59833c05933c00726ce12248531a2d05
Giacomo C. <giacomo.corso@gmail.com>
2024-10-11 19:53:24 +0200

    init ex 34

---
+++ Assets/Scripts/Ex_34.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_34 : MonoBehaviour
+{
+    /*
+     * Crear una esfera en el punto Y=10 y un
+     * suelo en el Y=0. Hacer que la esfera caiga al 
+     * pulsar el espacio. La esfera tiene que rebotar
+     * como un balón de baloncesto.
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
