e7d2b9e63e0b8469671c0177eebd8c93d2e59c4f
Giacomo C. <giacomo.corso@gmail.com>
2024-12-06 18:59:01 +0100

    init ex 39

---
+++ Assets/Scripts/30/Ex_39.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_39 : MonoBehaviour
+{
+    /*
+     * Crea unos cuantos objetos con el tag <<newton>>.
+     * Al pulsar espacio, debe activarse la gravedad de todos esos objetos.
+     * Los objetos no pueden tener ningún script añadido.
+     * El ejercicio tiene que funcionar sin modificar el 
+     * código aunque se añadan o quiten objetos de la escena.
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
