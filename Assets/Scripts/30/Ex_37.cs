0520ce4ea7fc0d413251efdc1ab9c382c76a0ba1
Giacomo C. <giacomo.corso@gmail.com>
2024-12-06 16:22:14 +0100

    init ex 37

---
+++ Assets/Scripts/30/Ex_37.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_37 : MonoBehaviour
+{
+    /*
+     * Crea una esfera en reposos sobre un suelo.
+     * Al pulsar la tecla S, recibe una fuerza puntual
+     * hacia arriba y hacia la derecha.
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
