946805da75d15c758b2d8c481262d8d5f4028b11
Giacomo C. <giacomo.corso@gmail.com>
2024-12-06 18:38:14 +0100

    ex 38 done

--- Assets/Scripts/30/Ex_38.cs
+++ Assets/Scripts/30/Ex_38.cs
@@ -9,16 +9,36 @@ public class Ex_38 : MonoBehaviour
      * Mientras la tecla S permanezca pulsada, la
      * esfera debe recibir una fuerza constante hacia arriba y hacia la derecha.
      */
+
+    Rigidbody rb;
+    private float m_Thrust = 10f;
+    private float _mass = 0.8f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.mass = _mass;
     }
 
-    // Update is called once per frame
+    /*
+     * FiedUpdate garantiza un intervalo de tiempo fijo entre fotogramas.
+     * Diseñado para trabajar con físicas y movimientos de objetos
+     * 
+     */
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector3(1, 1) * m_Thrust, ForceMode.Force);
+        }
+    }
+
+    /*
+     * Update is called once per frame. Pero no garantiza un intervalo de tiempo constante entre cada llamada.
+     * Ideal para entradas del usuario, como movimientos del teclado o clics del ratón.
+     */
     void Update()
     {
-        
     }
 }
