b244910741f2fe498a2667d559cd9a7e4131e059
Giacomo C. <giacomo.corso@gmail.com>
2024-12-06 18:16:56 +0100

    init ex 38

---
+++ Assets/Scripts/30/Ex_38.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_38 : MonoBehaviour
+{
+    /*
+     * Crea una esfera en reposo sobre un suelo.
+     * Mientras la tecla S permanezca pulsada, la
+     * esfera debe recibir una fuerza constante hacia arriba y hacia la derecha.
+     */
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
